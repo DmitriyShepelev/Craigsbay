8a1bc4e0359e51e05924edc599413f83a7178623
jimsupawish <jimsupawish@gmail.com>
2021-12-01 16:04:01 -0800

    Add a blank-slate database to the database file backend_db.db and modify tables.sql to contain a valid SQL to create the database.

--- tables.sql
+++ tables.sql
@@ -1,31 +1,34 @@
-CREATE TABLE Items {
-  item_id INT PRIMARY KEY AUTOINCREMENT,
-  quantity INT,
-  price FLOAT,
+CREATE TABLE Items (
+  item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  quantity INTEGER,
+  price REAL,
   item_name TEXT,
   category TEXT,
   description TEXT
-}
+);
 
-CREATE TABLE Accounts {
+CREATE TABLE Accounts (
   user_name TEXT PRIMARY KEY NOT NULL,
   user_password TEXT,
   email TEXT
-}
+);
 
-CREATE TABLE Feedbacks {
-  feedback_id INT PRIMARY KEY AUTOINCREMENT,
-  FOREIGN KEY (item_id) REFERENCES Items(item_id),
-  score INT,
+CREATE TABLE Feedbacks (
+  feedback_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  item_id INTEGER,
+  user_name TEXT,
+  score INTEGER,
   feedback_text TEXT,
-  FOREIGN KEY (user_name) REFERENCES Accounts(user_name)
-}
-
-CREATE TABLE Transanctions {
-  transanction_id INT PRIMARY KEY AUTOINCREMENT,
-  FOREIGN KEY (user_name) REFERENCES Accounts(user_name),
   FOREIGN KEY (item_id) REFERENCES Items(item_id),
-  total_price FLOAT,
-  transanction_date TEXT
-}
+  FOREIGN KEY (user_name) REFERENCES Accounts(user_name)
+);
 
+CREATE TABLE Transanctions (
+  transanction_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_name TEXT,
+  item_id INTEGER,
+  total_price REAL,
+  transanction_date TEXT,
+  FOREIGN KEY (user_name) REFERENCES Accounts(user_name),
+  FOREIGN KEY (item_id) REFERENCES Items(item_id)
+);
