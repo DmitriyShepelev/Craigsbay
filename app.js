0b0139366fb71bdbc81f195a8de23a3118afc0d5
jimsupawish <jimsupawish@gmail.com>
2021-12-02 23:51:56 -0800

    Now, /items endpoint seems to work! Import express and add in some npm installs so that running nodemon works.

--- app.js
+++ app.js
@@ -5,19 +5,19 @@
  * TODO: Add comments.
  */
 
-'use strict';
+"use strict";
 
 const express = require('express');
-
 const multer = require('multer');
-
 const sqlite3 = require('sqlite3');
-
 const sqlite = require('sqlite');
 
 const app = express();
 
 const SPECIFIED_PORT = 8000;
+const SERVER_ERROR_NUM = 500;
+const REQUEST_ERROR_NUM = 400;
+
 
 // For application/x-www-form-urlencoded.
 app.use(express.urlencoded({extended: true})); // Built-in middleware.
@@ -29,6 +29,36 @@ app.use(express.json()); // Built-in middleware.
 app.use(multer().none()); // Requires the "multer" module.
 
 
+/**
+ * Get all the items we have in the database if 
+ * Otherwise, get all of the yips with a text that contains the value associated with
+ * the "search" query paramter.
+ */
+app.get("/items", async (req, res) => {
+  try {
+    let resultItems = await getItemsFromTable();
+    res.json(resultItems);
+  } catch (err) {
+    console.log(err);
+    res.type("text");
+    res.status(SERVER_ERROR_NUM).send("An error occurred on the server. Try again later.");
+  }
+});
+
+
+/**
+ * Get all of the items from the Items table.
+ * @returns {JSONObject} the JSON object representing all the yips that we get from
+ *                       the table based on searchQuery.
+ */
+ async function getItemsFromTable() {
+  let db = await getDBConnection();
+  let dbResult = await db.all("SELECT * FROM Items");
+
+  await db.close();
+  return dbResult;
+}
+
 
 /**
  * Establishes a database connection to the database and returns the database
@@ -38,7 +68,7 @@ app.use(multer().none()); // Requires the "multer" module.
  */
 async function getDBConnection() {
   const db = await sqlite.open({
-    filename: 'yipper.db',
+    filename: "backend_db.db",
     driver: sqlite3.Database
   });
   return db;
