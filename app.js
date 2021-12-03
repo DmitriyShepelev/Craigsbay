1eb95c7108275fee909ed8d4b9fc596024894c6c
jimsupawish <jimsupawish@gmail.com>
2021-12-03 00:39:11 -0800

    Add in endpoint for individual item (/item).

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ const app = express();
 const SPECIFIED_PORT = 8000;
 const SERVER_ERROR_NUM = 500;
 const REQUEST_ERROR_NUM = 400;
-const AVG_SCORE_QUERY = "SELECT AVG(score) AS avg_score FROM Feedbacks WHERE Feedbacks.item_id = ?";
-
+const FULL_SCORE = 5;
+const AVG_SCORE_QUERY = "SELECT AVG(score) AS avg_score FROM Feedbacks WHERE item_id = ?";
+const GET_ITEM_FEEDBACK_QUERY = "SELECT user_name, score, feedback_text " +
+    "FROM Feedbacks WHERE item_id = ?";
 
 // For application/x-www-form-urlencoded.
 app.use(express.urlencoded({extended: true})); // Built-in middleware.
@@ -29,7 +31,6 @@ app.use(express.json()); // Built-in middleware.
 // For multipart/form-data (required with FormData).
 app.use(multer().none()); // Requires the "multer" module.
 
-
 /**
  * Get all the items we have in the database.
  */
@@ -38,38 +39,86 @@ app.get("/items", async (req, res) => {
     let resultItems = await getItemsFromTable();
     res.json(resultItems);
   } catch (err) {
-    console.log(err);
     res.type("text");
     res.status(SERVER_ERROR_NUM).send("An error occurred on the server. Try again later.");
   }
 });
 
+/**
+ * Get the more detailed information about an item specified by the "itemID".
+ */
+app.get("/item/:itemID", async (req, res) => {
+  try {
+    let itemID = req.params.itemID;
+    let resultItem = await getItemFromTable(itemID);
+
+    if (!resultItem) {
+      res.type("text");
+      res.status(REQUEST_ERROR_NUM).send("Item with id of " + itemID + " does not exist.");
+    }
+    res.json(resultItem);
+  } catch (err) {
+    res.type("text");
+    res.status(SERVER_ERROR_NUM).send("An error occurred on the server. Try again later.");
+  }
+});
 
 /**
  * Get all of the items from the Items table, along with their average score.
- * @returns {JSONObject} the JSON object representing all the items that we get from
+ * @returns {JSONObject} the JSON array representing all the items that we get from
  *                       the table based on searchQuery.
  */
- async function getItemsFromTable() {
+async function getItemsFromTable() {
   let db = await getDBConnection();
   let dbResult = await db.all("SELECT item_id, quantity, price, category, item_name FROM Items");
 
   for (let i = 0; i < dbResult.length; i++) {
-    let currItemID = dbResult[i].item_id;
-
-    let currAvgScore = await db.get(AVG_SCORE_QUERY, [currItemID]);
-
-    if (!currAvgScore.avg_score) {
-      currAvgScore = 5;
-    }
-
-    dbResult[i].avg_score = currAvgScore;
+    dbResult[i]["avg_score"] = await getAverageScore(dbResult[i].item_id);
   }
 
   await db.close();
   return dbResult;
 }
 
+/**
+ * Get more detailed information about an item from Items table, with an id specified by "itemID".
+ * Return undefined if no item with id of "itemID" exists in the table.
+ * @param {Number} itemID - the item id of the item that we want to get more information from
+ * @returns {JSONObject} the JSON object representing an item with an id itemID, along with
+ *                       the feedbacks of the users for that item. If there is no item with id of
+ *                       "itemID", undefined is returned.
+ */
+async function getItemFromTable(itemID) {
+  let db = await getDBConnection();
+  let oneItemInfo = await db.get("SELECT * FROM Items WHERE item_id = ?", [itemID]);
+
+  if (oneItemInfo) {
+    oneItemInfo["avg_score"] = await getAverageScore(itemID);
+    oneItemInfo["feedbacks"] = await db.all(GET_ITEM_FEEDBACK_QUERY, [itemID]);
+  }
+
+  db.close();
+  return oneItemInfo;
+}
+
+/**
+ * Get the average user score of an item with an item id of "itemID". Returns full score (5) if
+ * an item doesn't have any user score.
+ * @param {Number} itemID - the item id of the item that we want to get the average user score from
+ * @returns {Number} the average user score of an item, or 5 if there is no user score yet for
+ *                   current item with an id of "itemID"
+ */
+async function getAverageScore(itemID) {
+  let db = await getDBConnection();
+  let avgScore = await db.get(AVG_SCORE_QUERY, [itemID]);
+
+  if (!avgScore.avg_score) {
+    avgScore = FULL_SCORE;
+  }
+
+  db.close();
+  return avgScore;
+}
 
 /**
  * Establishes a database connection to the database and returns the database
